8de3461c91d91342838d81d88e1d70d647efa2ab
Bulbul-1998 <53138812+Bulbul-1998@users.noreply.github.com>
2020-06-18 22:46:14 +0530

    Update and rename pattern5.java to Pattern5.java

--- logic_building/Pattern5.java
+++ logic_building/Pattern5.java
@@ -1,14 +1,14 @@
-package ;
+package tech;
 
 import java.util.Scanner;
 
-public class Ex_35_Pattern5 {
+public class Pattern5 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int num,i,j,k,count,r;
 		//Input the number of lines you lines you want to display above and below in your pattern
-	    System.out.println("Enter number of lines you want to display above and below in your pattern: ");
+	    System.out.println("Enter number of lines you want to display : ");
 	    num=sc.nextInt();
 	    count=0;
 	    k=2;
