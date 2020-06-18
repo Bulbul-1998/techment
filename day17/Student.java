672ba3e8a93f759433e978a3c5929980b9fc8259
Bulbul-1998 <53138812+Bulbul-1998@users.noreply.github.com>
2020-06-18 23:15:40 +0530

    Create Student.java

---
+++ day17/Student.java
@@ -0,0 +1,26 @@
+package com.techment;
+
+public class Students {
+	private int Id;
+	private String name;
+	private String dept;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		this.Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+}
