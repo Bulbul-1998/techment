b1e2682c632285e9e122012f59d90365f50d515b
Bulbul-1998 <53138812+Bulbul-1998@users.noreply.github.com>
2020-06-24 23:41:53 +0530

    Create Form.java

---
+++ day15/Form.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class Form
+ */
+@WebServlet("/myjavafile1")
+public class Form extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Gson gson=new Gson();
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Employee emp =new Employee();
+		emp.setId(1);
+		emp.setName(request.getParameter("user"));
+		
+		String empjsonstring = gson.toJson(emp);
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		out.print(empjsonstring);
+	}
+
+}
